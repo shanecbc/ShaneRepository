1a8b0a12f55fd704c2faa0a3717baf6fcd8f428f
shanecbc <65754145+shanecbc@users.noreply.github.com>
2020-05-21 22:57:21 -0700

    Add files via upload

---
+++ ShanePyNmap.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 20:58:47 2020
+
+@author: Murphy
+"""
+
+
+import socketserver
+class MyTCPHandler(socketserver.BaseRequestHandler):   
+ """    
+The request handler class for our server.    
+It is instantiated once per connection to the server, and must    
+override the handle() method to implement communication to the    
+client.    """
+def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 8500
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
+
+import socket
+
+HOST, PORT = "localhost", 8500
+data = (" Shane Murphy")
+# Create a socket (SOCK_STREAM means a TCP socket)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
+
+
+
+
+
+
+
+
+
+
+
